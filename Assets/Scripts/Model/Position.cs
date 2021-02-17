79e0942c3656c35322dbde3716d7bab521e2293d
Данил Посохов <Iamposox@yandex.ru>
2021-02-17 13:37:33 +0300

    Удаление ненужных using

--- Assets/Scripts/Model/Position.cs
+++ Assets/Scripts/Model/Position.cs
@@ -2,13 +2,14 @@
 
 namespace Assets.Model {
     public class Position {
+        /// <summary>
+        /// Variables for future vector size assignment
+        /// </summary>
         private static float _xSize { get; set; }
         private static float _ySize { get; set; }
         private static float _zSize { get; set; }
 
         public Position() { }
-
-        public static Vector3 GetPosition(float xSize, float ySize, float zSize) => new Vector3(xSize, ySize, zSize);
 
         public static Vector3 RandomPosition(float xSize, float ySize, float zSize) => new Vector3(Random.Range(xSize*-1,xSize), ySize, Random.Range(zSize * -1, zSize));
     }
